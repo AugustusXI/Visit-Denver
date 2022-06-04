4b1db386a0d238023ad6df97341ca9bd83cdef20
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-06-04 15:58:04 -0600

    setup modal and buttons from materialize for filter and favorites

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
 $(".dropdown-trigger").dropdown();
+
+
+
+
+// Filter Modal JavaScript Code
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+  // Favorites List Modal JavaScript Code
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+//   });
+
+
